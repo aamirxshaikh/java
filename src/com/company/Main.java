f775aa01b29b8d45177d4530309a1a411f567263
Aamir shaikh <aamirshaikh3232@gmail.com>
2021-07-13 20:47:01 +0530

    Date and Time

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,12 @@
 package com.company;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello");
+        System.out.println(LocalDate.now().getMonth());
+        System.out.println(LocalTime.now().getHour());
     }
 }
