82dd22fdbbb68e277a375a01fb9912f393b8718d
Aamir shaikh <aamirshaikh3232@gmail.com>
2021-07-15 00:46:11 +0530

    Import conflict

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,24 +1,13 @@
 package com.company;
 
+import java.util.Date;
+
 public class Main {
 
     public static void main(String[] args) {
-        Item i1 = new Item("1");
-        Item i2 = i1;
+        Date date = new Date();
+        java.sql.Date sql = new java.sql.Date(3);
 
-        System.out.println("i1 = " + i1.title + " i2 = " + i2.title);
-
-        i1.title = "i1 change";
-        i2.title = "i2 change";
-
-        System.out.println("i1 = " + i1.title + " i2 = " + i2.title);
-    }
-
-    static class Item {
-        String title;
-
-        Item(String title) {
-            this.title = title;
-        }
+        System.out.println(sql);
     }
 }
