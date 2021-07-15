16b5fc404302069a08f1b84f745e0c70ca48d5b9
Aamir shaikh <aamirshaikh3232@gmail.com>
2021-07-15 20:48:53 +0530

    For, foreach and enhanced foreach loop

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,5 +1,6 @@
 package com.company;
 
+import java.util.Arrays;
 import java.util.Date;
 
 public class Main {
@@ -7,6 +8,18 @@ public class Main {
     public static void main(String[] args) {
         Date date = new Date();
         java.sql.Date sql = new java.sql.Date(3);
+
+        int [] items = {1, 2, 3, 4, 5, 10, 20, 22};
+
+        for (int i = 0; i < items.length; i++) {
+            System.out.println(items[i]);
+        }
+
+        for (int item : items) {
+            System.out.println(item);
+        }
+
+        Arrays.stream(items).forEach(System.out::println);
 
         System.out.println(sql);
     }
