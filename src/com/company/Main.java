8604425292f294260056c10b70f7f267440046b4
Aamir shaikh <aamirshaikh3232@gmail.com>
2021-07-17 16:45:23 +0530

    Calculating product discount

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
+        float priceAfterDiscount = (float) 0;
         Scanner sc = new Scanner(System.in);
 
         System.out.println("Enter product name");
@@ -15,5 +16,14 @@ public class Main {
 
         System.out.println("Discount ? (specify using true/false)");
         boolean discount = sc.nextBoolean();
+
+        if (discount) {
+            System.out.println("Enter %");
+            float percent = sc.nextFloat();
+
+            priceAfterDiscount = (price - (1/percent) * price);
+        }
+
+        System.out.println("Product : " + "'" + name + "'" + " Price before discount : " + price + " after discount : " + priceAfterDiscount);
     }
 }
