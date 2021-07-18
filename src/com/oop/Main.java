e1baaaae309071510e538fa17971930b765739a3
Aamir shaikh <aamirshaikh3232@gmail.com>
2021-07-18 15:41:37 +0530

    Classes and objects

---
+++ src/com/oop/Main.java
@@ -0,0 +1,28 @@
+package com.oop;
+
+import java.time.LocalDate;
+
+public class Main {
+    public static void main(String[] args) {
+        Product p1 = new Product("Product 1", "Accessories", LocalDate.of(2020, 3,  3), 50, true);
+
+        System.out.println(p1.name);
+        System.out.println(p1.dateOfManufacture);
+    }
+
+    static class Product {
+        String name;
+        String type;
+        LocalDate dateOfManufacture;
+        int price;
+        boolean isAvailable;
+
+        Product(String name, String type, LocalDate dateOfManufacture, int price, boolean isAvailable) {
+            this.name = name;
+            this.type = type;
+            this.dateOfManufacture = dateOfManufacture;
+            this.price = price;
+            this.isAvailable = isAvailable;
+        }
+    }
+}
