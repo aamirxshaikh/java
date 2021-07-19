d0801bb350c4f7e2771d36dceeac605ea63f24eb
Aamir shaikh <aamirshaikh3232@gmail.com>
2021-07-19 18:55:28 +0530

    Method parameters

---
+++ src/com/add/Main.java
@@ -0,0 +1,17 @@
+package com.add;
+
+public class Main {
+    public static void main(String[] args) {
+        int i = 10;
+
+        int result = add(10, 10, i);
+
+        System.out.println(result);
+    }
+
+    public static int add (int a, int b, int i) {
+        i = 20;
+
+        return a + b + i;
+    }
+}
