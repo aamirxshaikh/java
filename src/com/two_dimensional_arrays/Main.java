316e224fddcc2a9c365cbfad8e6178c8c2969f93
Aamir shaikh <aamirshaikh3232@gmail.com>
2021-07-22 21:10:33 +0530

    Declaring and Accessing two-d arrays

--- src/com/two_dimensional_arrays/Main.java
+++ src/com/two_dimensional_arrays/Main.java
@@ -2,6 +2,14 @@ package com.two_dimensional_arrays;
 
 public class Main {
     public static void main(String[] args) {
+        int[][] twoDInt = {{1, 2, 3}, {4, 5, 6}};
+        String[][] twoDString = {{"Hi"}, {"Hello"}};
+        boolean[][] twoDBoolean = {{true, false, true}, {false, false}};
+
+        System.out.println(twoDInt[0][2]);
+        System.out.println(twoDString[1][0]);
+        System.out.println(twoDBoolean[1][1]);
+
         for (int outer = 0; outer < 4; outer ++) {
             System.out.println("Outer index : " + outer);
 
