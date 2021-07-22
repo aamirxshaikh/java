48a3b0da30abdf2f6dfdc5a82ea08db39a09050f
Aamir shaikh <aamirshaikh3232@gmail.com>
2021-07-22 20:58:52 +0530

    Nested iteration statements

---
+++ src/com/two_dimensional_arrays/Main.java
@@ -0,0 +1,13 @@
+package com.two_dimensional_arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        for (int outer = 0; outer < 4; outer ++) {
+            System.out.println("Outer index : " + outer);
+
+            for (int inner = 0; inner < 5; inner ++) {
+                System.out.println("Inner index : " + inner);
+            }
+        }
+    }
+}
