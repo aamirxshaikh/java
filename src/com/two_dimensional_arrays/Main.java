8485077e67575b405e2c4607d9df678e0a4e67dc
Aamir shaikh <aamirshaikh3232@gmail.com>
2021-08-04 19:42:28 +0530

    Accessing two-d array

--- src/com/two_dimensional_arrays/Main.java
+++ src/com/two_dimensional_arrays/Main.java
@@ -2,20 +2,14 @@ package com.two_dimensional_arrays;
 
 public class Main {
     public static void main(String[] args) {
-        int[][] twoDInt = {{1, 2, 3}, {4, 5, 6}};
-        String[][] twoDString = {{"Hi"}, {"Hello"}};
-        boolean[][] twoDBoolean = {{true, false, true}, {false, false}};
+        String[][] arr = { {"A", "B", "C"}, {"D", "E", "F"}, {"G", "H", "I"} };
 
-        System.out.println(twoDInt[0][2]);
-        System.out.println(twoDString[1][0]);
-        System.out.println(twoDBoolean[1][1]);
-
-        for (int outer = 0; outer < 4; outer ++) {
-            System.out.println("Outer index : " + outer);
-
-            for (int inner = 0; inner < 5; inner ++) {
-                System.out.println("Inner index : " + inner);
+        for (int i = 0; i < arr.length; i ++) {
+            for (int j = 0; j < arr[i].length; j ++) {
+                System.out.print(arr[i][j] + " ");
             }
+
+            System.out.println();
         }
     }
 }
