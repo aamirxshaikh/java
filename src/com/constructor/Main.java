4ba82b59dfb3dc9a4aaeb9e1fbd482f2499cdd9a
Aamir shaikh <aamirshaikh3232@gmail.com>
2021-08-05 16:56:48 +0530

    Constructor overloading and toString overriding

--- src/com/constructor/Main.java
+++ src/com/constructor/Main.java
@@ -5,9 +5,11 @@ import java.time.LocalDate;
 public class Main {
     public static void main(String[] args) {
         Product p1 = new Product("Product 1", "Accessories", LocalDate.of(2020, 3,  3), 50, true);
-
+        Product p2 = new Product("Product 2", "Accessories",50, true);
         System.out.println(p1.name);
         System.out.println(p1.dateOfManufacture);
+        System.out.println(p1);
+        System.out.println(p2);
     }
 
     static class Product {
@@ -23,6 +25,17 @@ public class Main {
             this.dateOfManufacture = dateOfManufacture;
             this.price = price;
             this.isAvailable = isAvailable;
+        }
+
+        Product(String name, String type, int price, boolean isAvailable) {
+            this.name = name;
+            this.type = type;
+            this.price = price;
+            this.isAvailable = isAvailable;
+        }
+
+        public String toString () {
+            return this.dateOfManufacture + " " + this.name + " " + this.type + " " + this.price + " " + this.isAvailable;
         }
     }
 }
