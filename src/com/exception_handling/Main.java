7eea3be385eae3fed933cdb8276f91e5402fa14d
Aamir shaikh <aamirshaikh3232@gmail.com>
2021-08-10 23:23:02 +0530

    Exception handling

---
+++ src/com/exception_handling/Main.java
@@ -0,0 +1,22 @@
+package com.exception_handling;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int result;
+
+        try {
+            result = a / b;
+        } catch (ArithmeticException e) {
+            System.out.println(e.getMessage());
+        } finally {
+            sc.close();
+        }
+    }
+}
