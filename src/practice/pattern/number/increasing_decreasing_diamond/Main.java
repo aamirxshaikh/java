e683318db4f717de2faf7936c334cebe1f27b511
Aamir shaikh <aamirshaikh3232@gmail.com>
2021-09-09 23:05:31 +0530

    Practice - Pattern - Number - Increasing decreasing diamond

---
+++ src/practice/pattern/number/increasing_decreasing_diamond/Main.java
@@ -0,0 +1,39 @@
+package practice.pattern.number.increasing_decreasing_diamond;
+
+public class Main {
+    public static void main(String[] args) {
+        int n = 5, p = 1;
+
+        for (int i = 0; i < n - 1; i++, p ++) {
+            for (int j = i; j < n; j ++) {
+                System.out.print("  ");
+            }
+
+            for (int k = 0; k <= i; k ++) {
+                System.out.print(p + " ");
+            }
+
+            for (int l = 1; l <= i; l ++) {
+                System.out.print(p + " ");
+            }
+
+            System.out.println();
+        }
+
+        for (int i = 0; i < n; i++, p --) {
+            for (int j = 0; j <= i; j ++) {
+                System.out.print("  ");
+            }
+
+            for (int k = i; k < n; k ++) {
+                System.out.print(p + " ");
+            }
+
+            for (int l = i; l < n - 1; l ++) {
+                System.out.print(p + " ");
+            }
+
+            System.out.println();
+        }
+    }
+}
